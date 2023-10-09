23ea3dbb019ed02b5d02d79d01713860043b9455
LOX <139780480+l-ox@users.noreply.github.com>
2023-10-09 23:48:20 +0100

    Create loapi_server.js

---
+++ loapi_server.js
@@ -0,0 +1,38 @@
+const url = require('url');
+const http = require("http");
+const host = '10.1.0.4';
+const port = 80;
+const { exec } = require("child_process");
+command = "ping 8.8.8.8";
+var keys = ["5jEByz2Igr", "m9qd1PvdW8"];
+
+const requestListener = function (req, res) {
+        path = req.url
+        uri = ("http://" + host + ":8000" + path);
+        uri = new URL(uri);
+        const search_params = uri.searchParams;
+        const key = search_params.get('api_key');
+        const call = search_params.get('call');
+        if (keys.includes(key)) {
+                res.setHeader("Content-Type", "application/json");
+                res.writeHead(200);
+                if (call.includes("action1")) {
+                        call_status = "action1 success";
+                        exec(command);
+                }
+                else {
+                        call_status = "400 Bad Request";
+                }
+        res.end(`{"message": "200 OK - Authenticated Successfully", "call_status": "` + call_status + `"}`);
+        }
+        else {
+                res.setHeader("Content-Type", "application/json");
+                res.writeHead(401);
+                res.end(`{"message": "401 Unauthorised", "call_status": "N/A"}`);
+        }
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+        console.log(`Server is running on http://${host}:${port}`);
+});
